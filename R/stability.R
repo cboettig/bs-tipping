780d908bfbf8ec856f712cf8687db3fc58ef3015
rBatt <battrd@gmail.com>
2018-06-20 17:40:31 -0400

    update for solving for dJ/dt as func of J and params
    fix #5

--- R/stability.R
+++ R/stability.R
@@ -44,31 +44,42 @@ calcRoot_F <- function(pars){
 
 
 
-#' Temporal dynamics of F
+#' Temporal dynamics of F or J
 #' 
-#' Temporal dynamics of F, but as a function of F and parameters (not other state variables)
+#' Temporal dynamics of F or J, but as a function of F or J and parameters (not other state variables)
 #' 
-#' @param F abundance of planktivorous fish
+#' @param State0 abundance of planktivorous fish (F0) or abundance of juvenile bass (J0)
 #' @param pars named vector of parameters found in \code{\link{ecoStep}}. At a minimum, qE needs to be supplied. Optionally, other parameter arguments can be supplied here, but if they aren't, they are taken from the formals (defaults) of \code{\link{ecoStep}}.
+#' @param stateName a length-1 character indicating whether the value supplied (and value returned) pertains to "F0" (planktivores) or "J0" (juvenile bass). Juveniles ("J0") is chosen by default.
 #' 
-#' @return numeric value indicating temporal rate of change of F
+#' @return numeric value indicating temporal rate of change of F or J
 #' @examples
 #' getRoot(c(A0=1, F0=1, J0=1), pars=c(qE=0.5)) # find equilibria when bass are rare
-#' dF_dt_1state(F0=17.890184, pars=c(qE=0.5)) # F0 set to equilibrium when bass start as rare
-#' 
+#' dFJ_dt_1state(State0=17.890184, pars=c(qE=0.5), stateName="F0") # F0 set to equilibrium when bass start as rare
+#' dFJ_dt_1state(State0=3.06, pars=c(qE=0.5), stateName="J0") # J0 set to equilibrium when bass start as rare
+#'
 #' getRoot(c(A0=1000, F0=1, J0=1000), pars=c(qE=0.5)) # find equilibria when bass are abundant
-#' dF_dt_1state(F0=0.06712064, pars=c(qE=0.5)) # F0 set to equilibrium when bass start as abundant
-#' 
+#' dFJ_dt_1state(State0=0.06712064, pars=c(qE=0.5), stateName="F0") # F0 set to equilibrium when bass start as abundant
+#' dFJ_dt_1state(State0=992.5699, pars=c(qE=0.5), stateName="J0") # J0 set to equilibrium when bass start as abundant
+#'
 #' getRoot(c(A0=1, F0=1, J0=1), pars=c(qE=0.65)) # find equilibria when bass are rare
-#' dF_dt_1state(F0=100, pars=c(qE=0.65)) # F0 set to equilibrium when bass start as rare # WTF?!
+#' dFJ_dt_1state(State0=100, pars=c(qE=0.65), stateName="F0") # F0 set to equilibrium when bass start as rare # WTF?!
 #' fishStep(X=c(A0=6.275129e-18, F0=100, J0=1.443280e-17))
-#' 
+#' dFJ_dt_1state(State0=1.44e-17, pars=c(qE=0.65), stateName="J0") # J0 set to equilibrium when bass start as rare # Makes sense
+#'
 #' getRoot(c(A0=1000, F0=1, J0=1000), pars=c(qE=0.65)) # find equilibria when bass are abundant
-#' dF_dt_1state(F0=0.09136212, pars=c(qE=0.65)) # check planktivore equation for rate of change
+#' dFJ_dt_1state(State0=0.09136212, pars=c(qE=0.65), stateName="F0") # check planktivore equation for rate of change
+#' dFJ_dt_1state(State0=838.3849, pars=c(qE=0.65), stateName="J0") # check juvenile equation for rate of change
 #' fishStep(X=c(A0=364.51517642, F0=0.09136212, J0=838.38490576)) # re-examine rates of change of fish near equilibrium
 #' @export
-dF_dt_1state <- function(F0, pars){
-	
+dFJ_dt_1state <- function(State0, pars, stateName=c("J0","F0")){
+	stateName <- match.arg(stateName)
+	if(stateName=="J0"){
+		J0 <- State0
+	}
+	if(stateName=="F0"){
+		F0 <- State0
+	}
 	parsF <- unlist(formals(ecoStep)[c("fA", "cJA", "cJF", "cFA", "vuln", "hide", "surv", "Fo", "DF")])
 	
 	if(missing(pars)){ # if a function requires qE, pars needs to be supplied (e.g., pars=c(qE=0.1))
@@ -87,32 +98,22 @@ dF_dt_1state <- function(F0, pars){
 		h <- hide
 		s <- surv
 		d <- DF
-		
-		# d*(Fo-F) - a*F* ( (s*F)/(x*(qE+1-s)) - (z*v*F)/(x*(h+v+z*F)) ) # wrong
-		# d*(Fo-F) - a*F* ( (s*f)/(x*(qE+1-s)) - s/x - (z*v*F)/(x*(h+v+z*F)) )
-		d*(Fo-F0) - a*F0* ( (s*f)/(x*(qE+1-s)) - s/x - (z*v*F0)/(x*(h+v+z*F0)) )
-		
-		# Acalc = (s*J)/(qE+1-s)
-		# Fcalc = (d*Fo)/(d + a*A)
-		# Jcalc = (f*A)/(x*A + s + (z*v*F)/(h+v+z*F))
-		# A_JinA = (s*f)/(x*(qE+1-s)) - s/x - (z*v*F)/(x*(h+v+z*F)) # value for A assuming all states at equilibrium
-		# A_JinA2 = (s*Jcalc)/(qE+1-s)
-		# A_JinA3 = (s*((f*A)/(x*A + s + (z*v*F)/(h+v+z*F))))/(qE+1-s)
-		#
-		#
-		# A*(qE+1-s)*(x*A + s + (z*v*F)/(h+v+z*F)) == s*f*A
-		# (qE+1-s)*(x*A + s + (z*v*F)/(h+v+z*F)) == s*f
-		# x*A + s + (z*v*F)/(h+v+z*F) == (s*f)/(qE+1-s)
-		# A == (s*f)/(x*(qE+1-s)) - s/x - (z*v*F)/(x*(h+v+z*F))
-		#
-		# d*(Fo-F) - a*f*A
+				
+		if(stateName=="F0"){
+			dVal <- d*(Fo-F0) - a*F0* ( (s*f)/(x*(qE+1-s)) - s/x - (z*v*F0)/(x*(h+v+z*F0)) )
+		}
+		if(stateName=="J0"){
+			Q <- 1/(qE+1-s)
+			dVal <- f*s*J0*Q - x*J0^2*s*Q - s*J0 - (z*v*J0)/((h+v)/((d*Fo)/(d+a*s*J0*Q)) + z)
+		}
+
+		dVal
 		
 	})
 	
 	return(unname(out))
 	
 }
-
 
 
 #' Change in F temporal dynamics wrt F
